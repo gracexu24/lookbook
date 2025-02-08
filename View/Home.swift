72cb0474050829b2f5ae1dc270fec1270ebbefb0
thy055 <134018067+thy055@users.noreply.github.com>
2025-02-08 15:21:08 -0500

    kelly work

---
+++ View/Home.swift
@@ -0,0 +1,114 @@
+import SwiftUI
+
+struct Home: View {
+    //View Properties
+    @State private var searchText: String = ""
+    @State private var activeTab: Tab = .all
+    @Environment(\.colorScheme) private var scheme
+    @Namespace private var animation
+    var body: some View {
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 15 ) {
+                DummyMessagesView()
+            }
+            .safeAreaPadding(15)
+            .safeAreaInset(edge: .top, spacing: 0){
+                ExpandableNavigationBar()
+            }
+        }
+        .background(.gray.opacity(0.15))
+    }
+        
+
+        ///Expandable Navigation Bar
+        @ViewBuilder
+        func ExpandableNavigationBar(_ title: String = "Messages") -> some View {
+            GeometryReader { proxy in 
+            let minY = proxy.frame(in: .scrollView(axis: .vertical)).minY
+             VStack(spacing: 10) {
+                ///Title
+                Text(title)
+                    .font(.largeTitle.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 10)
+                
+                ///Search Bar
+                HStack(spacing: 12){
+                    Image(systemName: "magnifyingglass")
+                        .font(.title3)
+
+                        TextField("Search Conversation", text: Binding<String>)
+                }
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15)
+                .frame(height: 45)
+                .background {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(.background)
+                }
+
+                //Custom Segmented Picker
+                ScrollView(.horizontal){
+                    HStack(spacing: 12){
+                        ForEach(Tab.allCases, id:\.rawValue) { tab in
+                            Button(action: {
+                                withAnimation(.snappy) {
+                                    activeTab = tab
+                                }
+                            }) {
+                                Text(tab.rawValue)
+                                    .font(.callout)
+                                    .foregroundStyle(activeTab == tab ? (scheme == .dark ? .black
+                                        : .white) : Color.primary )
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 15)
+                                    .background{
+                                        if activeTab == tab {
+                                            Capsule()
+                                                .fill(Color.primary)
+                                                .matchedGeometryEffect(id: "ACTIVETAB", in:
+                                                    animation)
+                                        } else {
+                                            Capsule()
+                                                .fill(.background)
+                                        }
+                                    }
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }
+                }
+                .frame(height:50)
+            }
+            .padding(.top, 25)
+            .safeAreaPadding(.horizontal, 15)           
+            }
+            .frame(height: 190)
+            .padding(.bottom, 10)
+            .offset(y: minY < 0 ? -minY : 0)
+        }
+
+        ///Dummy Messages View
+        @ViewBuilder
+        func DummyMessagesView() -> some View {
+            ForEach(0..<20, id: \.self) { _ in
+                HStack(spacing: 12) {
+                    Circle()
+                        .frame(width: 55, height: 55)
+
+                    VStack(alignment: .leading, spacing: 6, content: {
+                        Rectangle()
+                            .frame(width: 140, height: 8)
+
+                        Rectangle()
+                            .frame(width: 140, height: 8)
+
+                        Rectangle()
+                            .frame(width: 80, height: 8)
+                    })
+                }
+                .foregroundStyle(.gray.opacity(0.4))
+                .padding(.horizontal, 15)
+            }
+        }
+    }
