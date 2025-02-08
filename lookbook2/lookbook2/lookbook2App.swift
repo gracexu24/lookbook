c9e52e0e4022fad5c344dd9939dbcbbfdea5a095
vivianknee <vivianni452@gmail.com>
2025-02-08 16:28:25 -0500

    vivian work

---
+++ lookbook2/lookbook2/lookbook2App.swift
@@ -0,0 +1,17 @@
+//
+//  lookbook2App.swift
+//  lookbook2
+//
+//  Created by Vivian  Ni on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct lookbook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
