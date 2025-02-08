b4811ff64f0fc75643f73d628dbe2ebe20016c36
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-02-08 15:31:53 -0500

    made a lookbook xcode project

---
+++ lookbook/lookbookTests/lookbookTests.swift
@@ -0,0 +1,16 @@
+//
+//  lookbookTests.swift
+//  lookbookTests
+//
+//  Created by Tejas Tirthapura on 2/8/25.
+//
+
+import Testing
+
+struct lookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
