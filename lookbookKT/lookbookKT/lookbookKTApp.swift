e0809ad60272a68c4c3b237b7ad70247d5cbe4cd
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-02-08 19:12:57 -0500

    kelly work

---
+++ lookbookKT/lookbookKT/lookbookKTApp.swift
@@ -0,0 +1,17 @@
+//
+//  lookbookKTApp.swift
+//  lookbookKT
+//
+//  Created by Tejas Tirthapura on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct lookbookKTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
